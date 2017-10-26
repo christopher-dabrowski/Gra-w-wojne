ea8cb8d4633d0f1e3bd6e17ed218b3d3ec6806fd
SiwyKrzysiek <k.fajny@gmail.com>
2017-10-26 18:10:14 +0200

    Funkcje ToString dla Karty Próba rozdawania i prototyp rozgrywki

--- Gra w wojne/main.c
+++ Gra w wojne/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string>
+#include <string.h>
+
+static int ROZMIAR_TALII = 52;
 
 enum Kolory
 {
@@ -34,26 +36,76 @@ struct Karta
 	enum Wartosci wartosc;
 };
 
+
+
 int Losowa(int mini, int maxi);
 void Resetuj(struct Karta *talia);
 void Tasuj(struct Karta *talia);
+const char * KolorToString(enum Kolory kolor);
+const char* WartoscToString(enum Wartosci wartosc);
+const char* KartaToString(struct Karta karta);
+void Rozdaj(struct Karta gracz1[ROZMIAR_TALII], struct Karta gracz2[ROZMIAR_TALII]);
+void Graj(struct Karta gracz[ROZMIAR_TALII], struct Karta komputer[ROZMIAR_TALII], int liczbaKartGracza, int liczbaKartKomputera);
+
 
 int main()
 {
-	srand(time(0));
-	struct Karta talia[52];
-	Resetuj(talia);
-	Tasuj(talia);
+	srand((uint)time(0));
+    
+    struct Karta gracz[ROZMIAR_TALII];
+    struct Karta komputer[ROZMIAR_TALII];
+    
+    Rozdaj(gracz, komputer);
+    puts(KartaToString(komputer[0]));
+    Graj(gracz, komputer, ROZMIAR_TALII/2, ROZMIAR_TALII/2);
 
-
-	for (int i = 0; i<52; i++)
-		printf("%d\n", talia[i].wartosc);
+	
 	return 0;
 }
 
 int Losowa(int mini, int maxi)
 {
 	return mini + rand() % (maxi - mini);
+}
+
+void Graj(struct Karta gracz[ROZMIAR_TALII], struct Karta komputer[ROZMIAR_TALII], int liczbaKartGracza, int liczbaKartKomputera)
+{
+    struct Karta stol[52];
+    int liczbaKartNaStole = 0;
+    
+    struct Karta wystawionaG = gracz[0];
+    struct Karta wystawionaK = komputer[0];
+    
+    stol[liczbaKartNaStole++] = wystawionaG;
+    stol[liczbaKartNaStole++] = wystawionaK;
+    
+    for (int i=1; i<52; i++)
+    {
+        gracz[i-1] = gracz[i];
+        komputer[i-1] = komputer[i];
+    }
+    
+    if (wystawionaG.wartosc > wystawionaK.wartosc)
+    {
+        
+    }
+    
+}
+
+void Rozdaj(struct Karta gracz1[ROZMIAR_TALII], struct Karta gracz2[ROZMIAR_TALII])
+{
+    struct Karta talia[ROZMIAR_TALII];
+    Resetuj(talia);
+    Tasuj(talia);
+    
+    for (int i=0; i<ROZMIAR_TALII/2; i++)
+    {
+        gracz1[i] = talia[i];
+    }
+    for (int i=ROZMIAR_TALII/2; i<ROZMIAR_TALII; i++)
+    {
+        gracz2[i] = talia[i];
+    }
 }
 
 void Resetuj(struct Karta *talia)
@@ -70,8 +122,7 @@ void Resetuj(struct Karta *talia)
 
 void Tasuj(struct Karta *talia)
 {
-	const int rozmiar = 52;
-	for (int i = rozmiar - 1; i>0; i--)
+	for (int i = ROZMIAR_TALII - 1; i>0; i--)
 	{
 		int j = Losowa(0, i + 1);
 		struct Karta tmp = talia[i];
@@ -79,3 +130,71 @@ void Tasuj(struct Karta *talia)
 		talia[j] = tmp;
 	}
 }
+
+const char* KolorToString(enum Kolory kolor)
+{
+    switch (kolor) {
+        case trefl:
+            return "Trefl";
+        case karo:
+            return "Karo";
+        case kier:
+            return "Kier";
+        case pik:
+            return "Pik";
+            
+        default:
+            return "Blad";
+            break;
+    }
+}
+
+const char* WartoscToString(enum Wartosci wartosc)
+{
+    switch (wartosc) {
+        case dwojka:
+            return "Dwojka";
+        case trojka:
+            return "Trojka";
+        case czworka:
+            return "Czworka";
+        case piatka:
+            return "Piatka";
+        case szustka:
+            return "Szustka";
+        case siudemka:
+            return "Siudemka";
+        case osemka:
+            return "Osemka";
+        case dziewiatka:
+            return "Dziewiatka";
+        case dziesiatka:
+            return "Dziesiatka";
+        case walet:
+            return "Walet";
+        case dama:
+            return "Dama";
+        case krol:
+            return "Krol";
+        case as:
+            return "As";
+            
+        default:
+            return "Blad";
+    }
+}
+
+const char* KartaToString(struct Karta karta)
+{
+    static char wynik[20];
+    strcpy(wynik, WartoscToString(karta.wartosc));
+    strcat(wynik, " ");
+    strcat(wynik, KolorToString(karta.kolor));
+    
+    return wynik;
+}
+
+
+
+
+
